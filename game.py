fee4fb67b0a24ad9092508ed9c7e090de0209bb2
Ali ACHENAN <ali.jallousy12@gmail.com>
2025-09-23 00:13:20 +0100

    Implemented player spawning and position updating

--- game.py
+++ game.py
@@ -1,6 +1,7 @@
 import pygame
 import pytmx
 import pyscroll
+from player import Player
 
 class Game:
 
@@ -13,10 +14,15 @@ class Game:
         tmx_data = pytmx.util_pygame.load_pygame('carte.tmx')
         map_data = pyscroll.data.TiledMapData(tmx_data)
         map_layer = pyscroll.orthographic.BufferedRenderer(map_data, self.screen.get_size())
-        # map_layer.zoom = 2
+        map_layer.zoom = 2
+
+        # generer un joueur
+        player_position = tmx_data.get_object_by_name("player_spawn")
+        self.player = Player(player_position.x, player_position.y)
 
         #dessiner les calques
-        self.group = pyscroll.PyscrollGroup(map_layer=map_layer, default_layer=1)
+        self.group = pyscroll.PyscrollGroup(map_layer=map_layer, default_layer=3)
+        self.group.add(self.player)
 
     def run(self):
         #active le jeu - boucle
@@ -24,6 +30,8 @@ class Game:
 
         while running:
 
+            self.group.update()
+            self.group.center(self.player.rect.center)
             self.group.draw(self.screen)
             pygame.display.flip()
 
